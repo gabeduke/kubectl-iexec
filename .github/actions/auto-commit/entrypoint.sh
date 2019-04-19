1af825449aa9c6f5fcdc8da5fed6bf8da9d10cd0
autobot <autobot@leetserve.com>
2019-04-18 23:24:09 -0400

    $*

--- .github/actions/auto-commit/entrypoint.sh
+++ .github/actions/auto-commit/entrypoint.sh
@@ -7,4 +7,4 @@ git config --global user.name 'autobot'
 git config --global user.email 'autobot@leetserve.com'
 git checkout -b "${BRANCH}"
 git add -A && git commit -m '$*' --allow-empty
-git push -u origin
+git push -u origin "${BRANCH}"
