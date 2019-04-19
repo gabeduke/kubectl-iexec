633f7bd664f4e4456a69dfc0069da244bf06535f
autobot <autobot@leetserve.com>
2019-04-19 04:58:44 +0000

    auto-commit

--- .github/actions/auto-commit/entrypoint.sh
+++ .github/actions/auto-commit/entrypoint.sh
@@ -7,11 +7,11 @@ git remote set-url origin https://gabeduke:${GITHUB_TOKEN}@github.com/gabeduke/k
 git config --global user.name 'autobot'
 git config --global user.email 'autobot@leetserve.com'
 git checkout -b "${BRANCH}"
-git add -A && git commit -m '$*' --allow-empty
+git add -A && git commit -m 'auto-commit' --allow-empty
 git push -u origin "${BRANCH}"
 
 curl -v \
     -u gabeduke:$GITHUB_TOKEN \
     -H "Content-Type:application/json" \
     -X POST https://api.github.com/repos/gabeduke/kubectl-iexec/pulls \
-    -d '{"title":"bumpver", "body": "bumpity bump", "head": "'"${BRANCH}"'", "base": "develop"}'
+    -d '{"title":"bumpver", "body": "bumpity bump", "head": "'${BRANCH}'", "base": "develop"}'
